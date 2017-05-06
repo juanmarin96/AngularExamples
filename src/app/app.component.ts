571f70d53fb5bd48cbc5a117c7e701952c060bbd
Juan <juanes-mejor@hotmail.com>
2017-05-06 00:06:28 -0500

    Subo la estructura del proyecto al repositorio de Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,67 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes
+} from '@angular/animations'
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <div class="row">
+        <div class="columns">
+            <button (click)="toggleMove()">Press me for animation</button>
+        </div>
+        <div class="columns">
+            <div id="content" [@focusPanel]='state' [@movePanel]='active'>Look at me animate</div>
+        </div>
+
+    </div>
+  `,
+  styles: [`
+        button{font-size: 1.8em;}
+        #content{padding:30px; background: #eeeeee;}
+      `],
+  animations: [
+    trigger('focusPanel', [
+      state('inactive', style({
+        transform: 'scale(1)',
+        backgroundColor: '#eee'
+
+      })),
+      state('active', style({
+        transform: 'scale(1.1)',
+        backgroundColor: '#cfd8dc'
+      })),
+
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+
+
+    ]),
+    trigger('movePanel', [
+
+      transition('void => *', [
+        animate(600, keyframes([
+          style({ opacity: 0, transform: 'translateY(-200px)', offset: 0 }),
+          style({ opacity: 1, transform: 'translateY(25px)', offset: .75 }),
+          style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+        ]))
+      ])
+
+
+
+
+    ])
+  ]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  state: string = 'inactive';
+  toggleMove() {
+    this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [BrowserModule,BrowserAnimationsModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
